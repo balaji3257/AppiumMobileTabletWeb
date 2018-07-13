89d72a2c5546e6fa027207b5995a4f0bc3ac2e04
QA <>
2018-07-13 16:07:25 +0530

    Added CustomeException

---
+++ src/com/genName/exceptionHandler/OwnException.java
@@ -0,0 +1,14 @@
+package com.genName.exceptionHandler;
+
+public class OwnException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8931272006605690602L;
+
+	public OwnException(String exceptionMessage) {
+		super(exceptionMessage);
+	}
+
+}
